a399a0254f5ad65d0e403bfb817da3f0f0edca20
butka <Bda.67@yandex.ru>
2022-05-28 22:00:35 +0300

    отформатирован жс и внесены правки после первого ревью ПР5

--- script/index.js
+++ script/index.js
@@ -59,78 +59,77 @@ const openPopup = (popupAny) => {
 const closePopup = (popupAny) => {
     popupAny.classList.remove('popup_opened');
   };
-  
 
-// функция редактирования данных из формы в профиль сохр
-function formSubmitHandler (evt) {
-    evt.preventDefault(); 
-    
-    const nameInputValue = nameInput.value;
-    const jobInputValue = jobInput.value;
-   
-    profileName.textContent = nameInputValue;
-    profileJob.textContent = jobInputValue;
+const createCard = ({ link, name }) => {
+  const cardTemplate = document.querySelector('.template').content;
+  const cardElement = cardTemplate.cloneNode(true);  
+  const cardName = cardElement.querySelector('.element__name');
+  cardName.textContent = name; 
+  const cardImage = cardElement.querySelector('.element__image');
+  cardImage.src = link;
+  cardImage.alt = name;
+  const buttonLike = cardElement.querySelector('.element__button-like');
+  const deleteCardBtn = cardElement.querySelector('.element__button-delete');
 
-    closePopup(popupEdit);
-};
-   const createCard = ({ link, name }) => {
-    const cardTemplate = document.querySelector('.template').content;
-    const cardElement = cardTemplate.cloneNode(true);  
-    const cardName = cardElement.querySelector('.element__name');
-    cardName.textContent = name; 
-    const cardImage = cardElement.querySelector('.element__image');
-    cardImage.src = link;
-    cardImage.alt = name;
-
-// Сделайте так, чтобы карточки можно было лайкать
-    const buttonLike = cardElement.querySelector('.element__button-like');
-    buttonLike.addEventListener('click', (evt) => {
-        (evt).preventDefault();
-        evt.target.classList.toggle('element__button-like_activated');
-    });
+// Сделайте так, чтобы карточки можно было лайкать 
+  buttonLike.addEventListener('click', (evt) => {
+    evt.target.classList.toggle('element__button-like_activated');
+  });
 
 // Добавьте карточкам иконку удаления. Настройте ее так, чтобы карточка удалялась при клике на эту иконку
-    const deleteCardBtn = cardElement.querySelector('.element__button-delete');
-    deleteCardBtn.addEventListener('click', (evt) => {
-        evt.target.closest('.element__item').remove();
-    });
+  deleteCardBtn.addEventListener('click', (evt) => {
+    evt.target.closest('.element__item').remove();
+  });
+
 // Настройте просмотр фотографий. Пусть открываются нажатием на картинку и закрываются кликом на крестик
-    cardImage.addEventListener('click', () => {
-        pictureOpened.src = link;
-        pictureOpened.alt = name;
-        pictureCaption.textContent = name;
-        openPopup(popupPicture); 
-    });
+  cardImage.addEventListener('click', () => {
+    pictureOpened.src = link;
+    pictureOpened.alt = name;
+    pictureCaption.textContent = name;
+    openPopup(popupPicture); 
+  });
 
-    return cardElement;
-   };
+  return cardElement;
+};
 
-    const renderCards = (card) => {
-    cardContainer.append(createCard(card));
-   };
+const renderCard = (card) => {
+  cardContainer.append( createCard(card) );
+};
 
-    initialCards.forEach((item) => {
-    renderCards(item);
-   });
+initialCards.forEach((item) => {
+  renderCard(item);
+});
 
 // Дайте пользователю возможность добавлять карточки. Сделайте так, чтобы при клике на «сохранить» новая карточка попадала в начало контейнера с ними. А диалоговое окно после добавления автоматически закрывалось.
 formElementCard.addEventListener('submit', (evt) => {
-    evt.preventDefault();
-    const newCard = createCard ({
+  evt.preventDefault();
+  const newCard = createCard ({
     name: cardNameInput.value,
     link: cardLinkInput.value
-    });
-    cardContainer.prepend(newCard);
-    closePopup(popupCard);
+  });
+  cardContainer.prepend(newCard);
+  closePopup(popupCard);
 });
 
 // слушатели
-formElementEdit.addEventListener('submit', formSubmitHandler);
+formElementEdit.addEventListener('submit', (evt) => {
+  evt.preventDefault(); 
+      
+  const nameInputValue = nameInput.value;
+  const jobInputValue = jobInput.value;
+     
+  profileName.textContent = nameInputValue;
+  profileJob.textContent = jobInputValue;
+  
+  closePopup(popupEdit);
+});
+
 openPopupEditBtn.addEventListener('click', () => {
    nameInput.value = profileName.textContent;
    jobInput.value = profileJob.textContent;
    openPopup(popupEdit); 
 });
+
 closePopupBtn.addEventListener('click', () => {
     closePopup(popupEdit);
 });
